0a753f701c9d4b64aafef0c9b4e88f29381dce35
Joachim Pileborg <arrow@pileborg.se>
2021-07-11 13:27:12 +0200

    Updated copyright year

--- src/host/posix/main.cpp
+++ src/host/posix/main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Joachim Pileborg
+// Copyright 2021 Joachim Pileborg
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
